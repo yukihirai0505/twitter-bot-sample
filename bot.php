8727e7b77be28b7934ef49bb5a5f3998aa27f209
Yuki Hirai <yukihirai0505@gmail.com>
2016-04-30 15:44:07 +0900

    Add get timeline sample

--- bot.php
+++ bot.php
@@ -17,19 +17,45 @@ $random = rand(0, count($messages)-1);
 $params = array(
     'status' => $messages[$random].'[固定メッセージ]'
   );
+/*
+post($url, $parameters = array()) 第一引数にURLのPATHを渡し、第二引数にパラメーターを渡す
+今回はこちらを使用
+https://dev.twitter.com/rest/reference/post/statuses/update
+他のURLも試してみる??
+下記を参照
+https://dev.twitter.com/rest/public
+*/
+
+/* 自分のタイムラインにツイート */
 $result = $conn->post('statuses/update',$params);
 
 if($result->text) {
   echo "【" . $result->text . "】とTwitterに投稿しました。";
 } else if(sizeof($result->errors) > 0) {
-  /* error message: Status is a duplicate. となっているときは同じ投稿を何回も繰り返すと返ってくるエラー */
+  /* error message: Status is a duplicate. は同じ投稿を何回も繰り返すと返ってくるエラー */
   $errors = $result->errors;
   foreach ($errors as $error) {
     echo "error message: " . $error->message;
   }
 } else {
-  echo "何か不測の事態が発生しているようだ。おれには手に負えないぜ";
+  echo "何か不測の事態が発生しているようだ。チャレンジングなことをしているようだな。おれには手に負えないぜ。そんな時は下記のvar_dumpでデバックしてみよう。";
 }
 
-/* デバッグしたいときは下記のコメントを外す */
+// デバッグしたいときは下記のコメントを外す
 // var_dump($result);
+
+
+/*
+自分のタイムラインを取得
+今回はこちらを使用
+https://dev.twitter.com/rest/reference/get/statuses/user_timeline
+*/
+/*
+$tweets = $conn->get('statuses/user_timeline', null);
+foreach($tweets as $index=>$tweet) {
+  echo $index+1 . "個目のツイート:" . $tweet->text . "<br />";
+}
+
+// デバッグしたいときは下記のコメントを外す
+// var_dump($tweets);
+*/
